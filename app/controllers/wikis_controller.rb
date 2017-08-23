43e6165a87b12b433edfd9f6cc3130f903441200
bsutt123 <bsutt123@gmail.com>
2017-08-23 16:37:53 -0400

    added in markdown rendering

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -9,6 +9,7 @@ class WikisController < ApplicationController
 
   def show
     @wiki = Wiki.find(params[:id])
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, fenced_code_blocks: true)
   end
 
   def delete
